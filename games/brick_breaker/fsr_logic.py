c9c8c0344d9fa4a6a37d1f9cd093f12b52d57b74
Al Roseweir <al.roseweir@girlguiding.org.uk>
2025-01-22 17:42:38 +0000

    breakbreaker in modules.

---
+++ games/brick_breaker/fsr_logic.py
@@ -0,0 +1,12 @@
+# Set up SPI for MCP3008
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1350000
+
+def read_adc(channel):
+    """Read data from the specified ADC channel."""
+    if channel < 0 or channel > 7:
+        return -1
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
---
+++ games/brick_breaker/media.py
@@ -0,0 +1,7 @@
+# Sounds
+paddle_hit_sound = pygame.mixer.Sound("sounds/ding.wav")
+brick_hit_sound = pygame.mixer.Sound("sounds/ding.wav")
+wall_hit_sound = pygame.mixer.Sound("sounds/ding.wav")
+life_lost_sound = pygame.mixer.Sound("sounds/tada.wav")
+game_over_sound = pygame.mixer.Sound("sounds/tada.wav")
+win_sound = pygame.mixer.Sound("sounds/chord.wav")
