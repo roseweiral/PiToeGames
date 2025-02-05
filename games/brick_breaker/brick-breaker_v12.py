077ebec1c9645526b0a58268a7c89343e5898502
Al Roseweir <al.roseweir@girlguiding.org.uk>
2025-02-05 22:12:40 +0000

    moved the while true logic for icp

--- games/brick_breaker/brick-breaker_v12.py
+++ games/brick_breaker/brick-breaker_v12.py
@@ -55,21 +55,6 @@ try:
     sensor1 = adafruit_icm20x.ICM20948(i2c, address=0x68)  # Address for Sensor 1
     sensor2 = adafruit_icm20x.ICM20948(i2c, address=0x69)  # Address for Sensor 2
 
-    while True:
-        # Read data from Sensor 1
-        accel_x1, accel_y1, accel_z1 = sensor1.acceleration
-        gyro_x1, gyro_y1, gyro_z1 = sensor1.gyro
-        mag_x1, mag_y1, mag_z1 = sensor1.magnetic
-
-        # Read data from Sensor 2
-        accel_x2, accel_y2, accel_z2 = sensor2.acceleration
-        gyro_x2, gyro_y2, gyro_z2 = sensor2.gyro
-        mag_x2, mag_y2, mag_z2 = sensor2.magnetic
-
-        # Print data from both sensors
-        print(f"Gyro X={gyro_x1:.2f}, Y={gyro_y1:.2f}, Z={gyro_z1:.2f},X={gyro_x2:.2f}, Y={gyro_y2:.2f}, Z={gyro_z2:.2f}")
-        #time.sleep(1)
-
 except (ImportError, FileNotFoundError):
     print("ICM Not Found. Running in simulation mode.")
     icr_simulation_mode = True
@@ -318,10 +303,24 @@ while True:
     print(f"FSR LEFT event detected: Value = {fsr_value_left}")
     handle_fsr_data(fsr_id="left", values=fsr_value_left)
 
-#    if fsr_value_right <> FSR_THRESHOLD:
+    #if fsr_value_right <> FSR_THRESHOLD:
     print(f"FSR RIGHT event detected: Value = {fsr_value_right}")
     handle_fsr_data(fsr_id="right", values=fsr_value_right)
     
+    # Read data from Sensor 1
+    accel_x1, accel_y1, accel_z1 = sensor1.acceleration
+    gyro_x1, gyro_y1, gyro_z1 = sensor1.gyro
+    mag_x1, mag_y1, mag_z1 = sensor1.magnetic
+
+    # Read data from Sensor 2
+    accel_x2, accel_y2, accel_z2 = sensor2.acceleration
+    gyro_x2, gyro_y2, gyro_z2 = sensor2.gyro
+    mag_x2, mag_y2, mag_z2 = sensor2.magnetic
+
+    # Print data from both sensors
+    print(f"Gyro X={gyro_x1:.2f}, Y={gyro_y1:.2f}, Z={gyro_z1:.2f},X={gyro_x2:.2f}, Y={gyro_y2:.2f}, Z={gyro_z2:.2f}")
+    time.sleep(1)
+
     # Event handling for keyboard
     keys = pygame.key.get_pressed()  # Get the state of all keys
     if keys[pygame.K_LEFT] and paddle.left > 0:  # Left arrow key
